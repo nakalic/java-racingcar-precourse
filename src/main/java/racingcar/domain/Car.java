9cd750552748f56a786a07ab84b3c92fbfa4d7bc
nakalic <nakalic28@gmail.com>
2022-12-13 21:41:40 +0900

    feat: 자동차가 현재 위치를 표현하는 기능 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -20,6 +20,14 @@ public class Car {
         return name;
     }
 
+    public String getNowPosition() {
+        String nowPosition = "";
+        for (int i = 0; i < position; i++) {
+            nowPosition += "-";
+        }
+        return nowPosition;
+    }
+
     private void validateNameSize(String name) {
         if (name.length() > 5) {
             throw new IllegalArgumentException("[ERROR] 자동차 이름은 5자 이하여야 합니다.");
