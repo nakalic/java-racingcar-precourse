85aa0c4b2074f2e1a8be330c0474532ded698060
nakalic <nakalic28@gmail.com>
2022-12-13 22:10:11 +0900

    feat: 실행결과 구문을 출력하는 기능 구현

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -3,6 +3,11 @@ package racingcar.view;
 import racingcar.domain.Car;
 
 public class OutputView {
+    public void printResult() {
+        System.out.println();
+        System.out.println("실행 결과");
+    }
+
     public void printCarAndPosition(Car car) {
         System.out.print(car + " : " + car.getNowPosition());
         System.out.println();
