d3679ec0a418e9ba216823e1feba2852b69ef3f4
nakalic <nakalic28@gmail.com>
2022-12-13 21:30:02 +0900

    feat: Car객체 toString() 메소드 오버라이딩

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -15,6 +15,11 @@ public class Car {
         }
     }
 
+    @Override
+    public String toString() {
+        return name;
+    }
+
     private void validateNameSize(String name) {
         if (name.length() > 5) {
             throw new IllegalArgumentException("[ERROR] 자동차 이름은 5자 이하여야 합니다.");
