113cf4724621dbdd8452254bff78cfd30b8665c9
nakalic <nakalic28@gmail.com>
2022-12-13 21:05:57 +0900

    feat: 시도 횟수를 입력받는 기능 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -10,4 +10,31 @@ public class InputView {
         String[] cars = Console.readLine().split(",");
         return new ArrayList<>(Arrays.asList(cars));
     }
+
+    public int inputTryCount() {
+        // TryCount 라는 클래스를 만드는 것은 오버엔지니어링일 것 같다.
+        String input = Console.readLine();
+        validateTryCount(input);
+        return Integer.parseInt(input);
+    }
+
+    private void validateTryCount(String input) {
+        checkNaturalNumber(input);
+        checkOverThanZero(input);
+    }
+
+    private void checkNaturalNumber(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException exception) {
+            throw new IllegalArgumentException("시도 횟수는 숫자여야합니다.");
+        }
+    }
+
+    private void checkOverThanZero(String input) {
+        if (Integer.parseInt(input) <= 0) {
+            throw new IllegalArgumentException("시도 횟수는 0보다 커야합니다.");
+        }
+    }
 }
+
