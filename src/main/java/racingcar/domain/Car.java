75396eab51cb529ead5caf8e32ffbe6aec0f6c1c
nakalic <nakalic28@gmail.com>
2022-12-13 20:50:57 +0900

    feat: 자동차 이름이 5글자 이하가 아닐시 IllegalArgumentException을 발생하도록 구현

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -5,8 +5,13 @@ public class Car {
     private int position = 0;
 
     public Car(String name) {
+        validateNameSize(name);
         this.name = name;
     }
 
-    // 추가 기능 구현
+    private void validateNameSize(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("[ERROR] 자동차 이름은 5자 이하여야 합니다.");
+        }
+    }
 }
