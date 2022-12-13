ee09bfa3e9c172a23ab4d22a7a2c6c989fe101b7
nakalic <nakalic28@gmail.com>
2022-12-13 23:16:38 +0900

    feat: 시도 횟수를 잘못 입력 했을 때 예외 메세지를 출력하고 다시 입력받도록 구현

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public class InputView {
     public List<String> inputCars() {
+        // 여기서 검증을 해줘야 하나? 계속 입력 어떻게 받지?
         System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
         String[] cars = Console.readLine().split(",");
         return new ArrayList<>(Arrays.asList(cars));
@@ -14,10 +15,16 @@ public class InputView {
 
     public int inputTryCount() {
         // TryCount 라는 클래스를 만드는 것은 오버엔지니어링일 것 같다.
-        System.out.println("시도할 회수는 몇회인가요?");
-        String input = Console.readLine();
-        validateTryCount(input);
-        return Integer.parseInt(input);
+        while(true) {
+            try {
+                System.out.println("시도할 회수는 몇회인가요?");
+                String input = Console.readLine();
+                validateTryCount(input);
+                return Integer.parseInt(input);
+            } catch (IllegalArgumentException exception) {
+                System.out.println(exception.getMessage());
+            }
+        }
     }
 
     private void validateTryCount(String input) {
